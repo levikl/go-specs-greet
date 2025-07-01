ca696923929f9bfdf5d8c9dfbdfddbd320ff3a46
Levi Klingler <levikl@geeked.gg>
2025-07-01 19:47:38 -0400

    chore: refactoring + consolidate `Dockerfile`s

--- cmd/grpcserver/main.go
+++ cmd/grpcserver/main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"context"
 	"log"
 	"net"
 
 	"google.golang.org/grpc"
 
 	"github.com/levikl/go-specs-greet/adapters/grpcserver"
-	"github.com/levikl/go-specs-greet/domain/interactions"
 )
 
 func main() {
@@ -17,20 +15,9 @@ func main() {
 		log.Fatal(err)
 	}
 	s := grpc.NewServer()
-	grpcserver.RegisterGreeterServer(s, &GreetServer{})
+	grpcserver.RegisterGreeterServer(s, &grpcserver.GreetServer{})
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatal(err)
 	}
-}
-
-type GreetServer struct {
-	grpcserver.UnimplementedGreeterServer
-}
-
-func (g GreetServer) Greet(
-	ctx context.Context,
-	request *grpcserver.GreetRequest,
-) (*grpcserver.GreetReply, error) {
-	return &grpcserver.GreetReply{Message: interactions.Greet(request.Name)}, nil
 }
