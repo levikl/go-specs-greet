e1ad8325470e85a429c420c7d8a67d2defbfad21
Levi Klingler <levikl@geeked.gg>
2025-07-01 22:13:28 -0400

    refactor: use `Curse` specific types in grpc adapter

--- adapters/grpcserver/server.go
+++ adapters/grpcserver/server.go
@@ -14,6 +14,6 @@ func (g GreetServer) Greet(ctx context.Context, request *GreetRequest) (*GreetRe
 	return &GreetReply{Message: interactions.Greet(request.Name)}, nil
 }
 
-func (g GreetServer) Curse(ctx context.Context, request *GreetRequest) (*GreetReply, error) {
-	return &GreetReply{Message: interactions.Curse(request.Name)}, nil
+func (g GreetServer) Curse(ctx context.Context, request *CurseRequest) (*CurseReply, error) {
+	return &CurseReply{Message: interactions.Curse(request.Name)}, nil
 }
