df794163c772fc89fa5dd7e12cd79e8819efbb17
Levi Klingler <levikl@geeked.gg>
2025-07-02 00:02:13 -0400

    feat: update the domain logic with a unit test

--- domain/interactions/greet_test.go
+++ domain/interactions/greet_test.go
@@ -3,6 +3,8 @@ package interactions_test
 import (
 	"testing"
 
+	"github.com/alecthomas/assert/v2"
+
 	"github.com/levikl/go-specs-greet/domain/interactions"
 	"github.com/levikl/go-specs-greet/specifications"
 )
@@ -12,4 +14,8 @@ func TestGreet(t *testing.T) {
 		t,
 		specifications.GreetAdapter(interactions.Greet),
 	)
+
+	t.Run("it defaults `name` to `\"World\"` when it is empty", func(t *testing.T) {
+		assert.Equal(t, "Hello, World", interactions.Greet(""))
+	})
 }
